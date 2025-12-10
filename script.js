565c6e59bb788b1e7a58494d006fe59c7846f6aa
Jorge Montes <jm18103797.jm@gmail.com>
2025-12-10 19:22:04 -0300

    Primeiro commit completo do portfólio

---
+++ script.js
@@ -0,0 +1,34 @@
+// Seleciona o botão e o conteúdo
+const btnToggle = document.getElementById("btn-toggle");
+const conteudo = document.getElementById("conteudo-bem-vindo");
+
+// Adiciona o evento de clique
+btnToggle.addEventListener("click", () => {
+  // Alterna a classe hidden
+  conteudo.classList.toggle("hidden");
+
+  // Muda o ícone de acordo com o estado
+  if (conteudo.classList.contains("hidden")) {
+    btnToggle.innerHTML = '<i class="fa-solid fa-chevron-down"></i>';
+  } else {
+    btnToggle.innerHTML = '<i class="fa-solid fa-chevron-up"></i>';
+  }
+});
+
+const form = document.getElementById("formContato");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const empresa = form.empresa.value.trim();
+  const vaga = form.vaga.value.trim();
+  const mensagem = form.mensagem.value.trim();
+  console.log(empresa, vaga, mensagem);
+
+  const corpoEmail = `Empresa: ${empresa} \nVaga: ${vaga} \nMensagem: ${mensagem}`;
+
+  form.action = `mailto:jm18103797.jm@gmail.com?subject=****Contato%20${empresa}%20Vaga%20${vaga}&body=${encodeURIComponent(
+    corpoEmail
+  )}`;
+  form.submit();
+});
